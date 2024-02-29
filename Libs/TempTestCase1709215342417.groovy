468ae93e59c5196aea7c2ac0aa766b0e17ecf7ae
Syidik <syidik.priam@gmail.com>
2024-02-29 21:12:04 +0700

    add result

--- Libs/TempTestCase1709215342417.groovy
+++ Libs/TempTestCase1709215342417.groovy
@@ -4,21 +4,21 @@ import com.kms.katalon.core.testcase.TestCaseBinding
 import com.kms.katalon.core.driver.internal.DriverCleanerCollector
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
-import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
 import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
-import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
 import com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
 
 
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\syidi\\AppData\\Local\\Temp\\Katalon\\20240226_152513\\execution.properties')
+RunConfiguration.setExecutionSettingFile('/tmp/Katalon/20240229_140222/execution.properties')
 
 TestCaseMain.beforeStart()
 
